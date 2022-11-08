728c79c6830dfb6ab6c48117071cf18d728dd7e2
Ethan <38486990+ByCubed7@users.noreply.github.com>
2022-11-08 10:08:51 +0000

    Uploaded script

---
+++ Bridge.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Links the events from the Server to the IRC Chat
+public class Bridge
+{
+    public IRC.Chat chat;
+
+    private Bridge(IRC.Chat chat)
+    {
+        this.chat = chat;
+    }
+
+    static public Bridge CreateServerBridge(IRC.Chat chat)
+    {
+        Bridge newBridge = new Bridge(chat);
+
+        chat.MessageSent += newBridge._Chat_MessageSent;
+        chat.MemberJoined += newBridge._Chat_MemberJoined;
+        chat.MemberLeft += newBridge._Chat_MemberLeft;
+
+        return newBridge;
+    }
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    private void _Chat_MessageSent(object? sender, IRC.MessageSentEventArgs e)
+    {
+        IRC.Message message = e.Message;
+        Console.WriteLine("[CLIENT] [" + message.SentAt.ToString("HH:mm:ss") + "] " + message.Author.UserName + ": " + message.Body);
+
+    }
+
+    private void _Chat_MemberJoined(object? sender, IRC.MemberJoinedEventArgs e)
+    {
+        Console.WriteLine("[Client] [" + DateTime.Now.ToString("HH:mm:ss") + "] " + e.Member.UserName + " Joined!");
+    }
+
+    private void _Chat_MemberLeft(object? sender, IRC.MemberLeftEventArgs e)
+    {
+        Console.WriteLine("[Client] [" + DateTime.Now.ToString("HH:mm:ss") + "] " + e.Member.UserName + " Left!");
+    }
+}
---
+++ GUI/ChatRoom.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class ChatRoom : Window
+    {
+
+        public ChatRoom()
+        {
+            InitializeComponent();
+
+            SendButton.MouseDown += SendButton_MouseDown;
+            ExitButton.MouseDown += ExitButton_MouseDown;
+        }
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        // Actions
+
+        public void AddMessage(string str)
+        {
+            MessageLabel.Content += '\n' + str;
+
+            // Scroll to bottom if near
+
+            // Play sounds?
+
+        }
+        
+
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        // Interactions
+
+        private void SendButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //MessageBox.Text;
+            InvokeSendMessage(new SendMessageEventArgs());
+        }
+
+        private void ExitButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            InvokeLeave(new LeaveEventArgs());
+            
+        }
+
+
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        // Events
+
+
+        // When the client attempts to send a message
+        public event EventHandler<SendMessageEventArgs> SendMessage;
+        protected virtual void InvokeSendMessage(SendMessageEventArgs e)
+        {
+            var handler = SendMessage;
+            handler?.Invoke(this, e);
+        }
+        public class SendMessageEventArgs : EventArgs { }
+
+
+        // When the client attempts to send a message
+        public event EventHandler<LeaveEventArgs> Leave;
+        protected virtual void InvokeLeave(LeaveEventArgs e)
+        {
+            var handler = Leave;
+            handler?.Invoke(this, e);
+        }
+        public class LeaveEventArgs : EventArgs { }
+    }
+}

89e376736449b7a962794a9a2ed0b0e491e3ce3f
Ethan <38486990+ByCubed7@users.noreply.github.com>
2022-11-08 21:58:49 +0000

    See prev

---
+++ IRC/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRC
+{
+    public class Message : SocketConnect.Message
+    {
+        public string Author { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
--- SocketConnect/Message.cs
+++ SocketConnect/Message.cs
@@ -8,28 +8,43 @@ namespace SocketConnect
 {
     public class Message
     {
-        string header;
-        List<string> args;
+        public string Header { get; set; }
+        public List<string> Args { get; set; }
+
+        public Message()
+        {
+            Header = "HEADER";
+            Args = new List<string>();
+        }
+
+        virtual public void FromString(string data)
+        {
+            List<string> datalist = new List<string>(data.Split(Connection.Split));
+
+            Header = datalist[0];
+            datalist.RemoveAt(0);
+
+            Args = datalist;
+        }
 
         public void FromBytes(byte[] data)
         {
-            string datastring = Encoding.UTF8.GetString(data);
-            List<string> datalist = new List<string>(datastring.Split(Connection.Split));
+            FromString(Encoding.UTF8.GetString(data));
+        }
 
-            header = datalist[0];
-            datalist.RemoveAt(0);
-            
-            args = datalist;
+        public override string ToString()
+        {
+            string datastring = Header + Connection.Split;
+            foreach (string arg in Args)
+                datastring += arg + Connection.Split;
+            datastring.Trim();
+            datastring += Connection.EOF;
+            return datastring;
         }
 
         public byte[] ToBytes()
         {
-            string datastring = header + Connection.Split;
-            foreach (string arg in args) 
-                datastring += arg + Connection.Split;
-            datastring.Trim();
-            datastring += Connection.EOF;
-            return Encoding.UTF8.GetBytes(datastring);
+            return Encoding.UTF8.GetBytes(ToString());
         }
 
 

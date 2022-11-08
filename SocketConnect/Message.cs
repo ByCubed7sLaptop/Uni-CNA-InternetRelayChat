8e71703904c5efcad61c4fe3b11bfafc9c193213
Ethan <38486990+ByCubed7@users.noreply.github.com>
2022-11-08 12:38:59 +0000

    Creating simple communication classes ready for sockets

---
+++ SocketConnect/Message.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketConnect
+{
+    public class Message
+    {
+        string header;
+        List<string> args;
+
+        public void FromBytes(byte[] data)
+        {
+            string datastring = Encoding.UTF8.GetString(data);
+            List<string> datalist = new List<string>(datastring.Split(Connection.Split));
+
+            header = datalist[0];
+            datalist.RemoveAt(0);
+            
+            args = datalist;
+        }
+
+        public byte[] ToBytes()
+        {
+            string datastring = header + Connection.Split;
+            foreach (string arg in args) 
+                datastring += arg + Connection.Split;
+            datastring.Trim();
+            datastring += Connection.EOF;
+            return Encoding.UTF8.GetBytes(datastring);
+        }
+
+
+    }
+}

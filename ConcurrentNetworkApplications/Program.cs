7e06e67ba7c05a7a0483012b13ef066b17688511
Ethan <38486990+ByCubed7@users.noreply.github.com>
2022-11-23 12:28:12 +0000

    Attempted GUI usage

--- ConcurrentNetworkApplications/Program.cs
+++ ConcurrentNetworkApplications/Program.cs
@@ -1,27 +1,47 @@
 ﻿using CubedChess;
+using System;
+using System.Windows;
 
-Console.WriteLine("Hello, ConcurrentNetworkApplications!");
+namespace ConcurrentNetworkApplications
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            
+            //GUI.MainMenu window = new GUI.MainMenu();
 
-Board board = new CubedChess.Board();
-
-board.DebugPrint();
-Console.WriteLine();
-
-board.Move(Board.b2, Board.b3);
-board.Move(Board.c1, Board.a3);
-board.Move(Board.a3, Board.b4);
-
-board.DebugPrint();
-Console.WriteLine();
-
-board.DebugPrintNumbers();
-Console.WriteLine();
-
-
-GUI.MainMenu window = new GUI.MainMenu();
+            //window.Title = "WPF in Console";
+            //window.Width = 400;
+            //window.Height = 300;
+            //window.ShowDialog();
 
 
 
+            Console.WriteLine("Hello, ConcurrentNetworkApplications!");
+
+            Board board = new Board();
+
+            board.DebugPrint();
+            Console.WriteLine();
+
+            board.Move(Board.b2, Board.b3);
+            board.Move(Board.c1, Board.a3);
+            board.Move(Board.a3, Board.b4);
+
+            board.DebugPrint();
+            Console.WriteLine();
+
+            board.DebugPrintNumbers();
+            Console.WriteLine();
 
 
-//m_Form.ShowDialog();
+
+
+            //m_Form.ShowDialog();
+        }
+    }
+}
+
+
